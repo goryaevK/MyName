eed2dd7e8822fd9a60a6d059117ad8121ffad09d
GKV <nov_gkv@mail.ru>
2025-01-31 17:28:42 +0500

    replace1

--- MyName/ViewController.swift
+++ MyName/ViewController.swift
@@ -9,19 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            // Создание кнопки
-            let button = UIButton(type: .system)
-            button.setTitle("Нажми меня", for: .normal)
-            button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
-            
-            // Добавление цели для кнопки
-            button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-            
-            // Добавление кнопки на экран
-            self.view.addSubview(button)
-        }
+        super.viewDidLoad()
+        
+        // Создание кнопки
+        let button = UIButton(type: .system)
+        button.setTitle("Нажми меня", for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        button.backgroundColor = .green
+        // Добавление цели для кнопки
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        
+        // Добавление кнопки на экран
+        self.view.addSubview(button)
+    }
         
     @objc func buttonPressed() {
         print("Кнопка была нажата!")
